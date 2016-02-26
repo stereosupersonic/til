0a00b4cd2ea7588bab921d2fa82e4b39550f93cf
Michael Deimel <michael@deimel.de>
2016-02-26 13:25:25 +0100

    Benchmark ruby code

---
+++ ruby/benchmark_code.rb
@@ -0,0 +1,26 @@
+# Benchmark ruby code
+
+## simple
+```
+Benchmark.bm(10) do |x|
+  x.report('f1:')         { function1 }
+  x.report('f2:')  { function2 }
+end
+
+```
+
+## better with benchmark/ips
+```
+gem "benchmark-ips"
+
+require 'benchmark/ips'
+user_ids = (1..5000).to_a
+
+Benchmark.ips do |x|
+  x.report("array: ")  { User.select(:id).where(emails_allowed: true).where("id != all(array[?])", user_ids).find_each {} }
+  x.report("not in: ") { User.select(:id).where(emails_allowed: true).where.not(id: user_ids).find_each {} }
+
+  x.compare!
+end
+
+``
